1b3d8e0509bba7144e77593dd42690fe3dc3f585
dsbasha <45025189+dsbasha@users.noreply.github.com>
2019-07-01 15:55:54 +0530

    Create List_tuple.py

---
+++ Python/List_tuple.py
@@ -0,0 +1,16 @@
+tup1 = 1,2,3,4,5,6
+print(tup1)
+type(tup1)
+
+tup2 = (78687,8759684,-4586045,"4645","Basha","Manish","Vimal",34534.10,True,'Basha')
+tup2
+tup2[2]
+tup2[1:5]
+tup2[3:]
+print(dir(tup1))
+
+list1 = [78687,8759684,-4586045,"4645","Basha","Manish","Vimal",34534.10,True]
+len(list1)
+
+# dir gives attributes and functions of perticular object i.e list, touple, srt, int etc.
+print(dir(list1))
