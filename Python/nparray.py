32b9754b4c80532356c72d899cd44d5f079abcfa
dsbasha <45025189+dsbasha@users.noreply.github.com>
2019-07-02 16:56:56 +0530

    Create nparray.py

---
+++ Python/nparray.py
@@ -0,0 +1,11 @@
+arr1 = np.array(list1)
+arr1[[2,1,3]]
+
+arr2=np.array([3,4,5,7,9,23,345,67,89,67,34,89,67,2,3,4,8,56,34])
+arr2
+
+arr2[(arr2>50)]
+
+arr3 = np.array(['test10','test2','test11','test4','test5','test6','test7','test8','test9','test1','test3'])
+arr3.sort()
+arr3
