f64f856710d64a24d0f103471ca1dfee9195e3ea
dsbasha <45025189+dsbasha@users.noreply.github.com>
2019-07-02 16:55:57 +0530

    Update case_study2.py

--- Python/case_study2.py
+++ Python/case_study2.py
@@ -27,3 +27,32 @@ def mult_table(x):
 num = int(input("Enter the number to show multiplication table of"))  
 mult_table(num)
 
+#Case Study 5
+s=''
+for i in range(2000,3201):
+    if i%7==0 and i%5==0:
+        s=s+str(i)+','
+print(s)
+
+#Case Study 6
+def area(len, shape):
+    area = 0
+    if shape.upper() == "CIRCLE":
+        area = 3.14*len*len
+    if shape.upper() == "SQUARE":
+        area = len*len
+    return area
+
+print("Area of the Square is: ",area(10,"square"))
+print("Area of the Circle is: ",area(10,"circle"))
+
+def area1(*a):
+    area = 0
+    if a[1].upper() == "CIRCLE":
+        area = 3.14*len*len
+    if a[1].upper() == "SQUARE":
+        area = len*len
+    return area 
+
+print("Area of the Square is: ",area(10,"square"))
+print("Area of the Circle is: ",area(10,"circle"))
